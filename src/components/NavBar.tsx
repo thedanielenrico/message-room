c5ec9d9277fbe36b3a6ff9a76bcb52ed1040cd53
Daniel Enrico <thedanielenrico@gmail.com>
2024-10-07 09:06:29 -0700

    initial commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <nav className="p-4 bg-blue-400">
+      <div className="text-center">
+        <h1 className="text-xl">Message Room</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
